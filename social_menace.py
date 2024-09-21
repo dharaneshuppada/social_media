ec92bbced9523ecd3c04dab62b4405f88a931a54
dharaneshuppada <dharaneshuppada@gmail.com>
2024-09-21 14:08:52 +0530

    Update social_menace.py

--- social_menace.py
+++ social_menace.py
@@ -1,7 +1,7 @@
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import streamlit as st
 
 # Load the data
 df = pd.read_csv("Time-Wasters on Social Media.csv")
